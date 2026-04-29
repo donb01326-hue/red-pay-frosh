805b59109346acae5ebe0cbfce987c72dd88e5a1
donb01326-hue <donb01326@gmail.com>
2026-04-29 17:51:37 +0100

    Update PaymentInstructions.tsx

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "6805281706";
-  const bankName = "fairmoney microfinace";
-  const accountName = "AROWOJOLU ABIDEMI";
+  const accountNumber = "8138324331";
+  const bankName = "PALMPAY";
+  const accountName = "OBADIAH RAPHAEL";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
